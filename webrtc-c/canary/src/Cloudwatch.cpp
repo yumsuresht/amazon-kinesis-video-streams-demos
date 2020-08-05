5294c9a7ff46c1c8d359a4bd74381fe548c1aff0
Lukas Herman <lherman.cs@gmail.com>
2020-08-05 11:57:44 -0700

    Refractor cloudwatch

--- webrtc-c/canary/src/Cloudwatch.cpp
+++ webrtc-c/canary/src/Cloudwatch.cpp
@@ -2,8 +2,7 @@
 
 namespace Canary {
 
-Cloudwatch::Cloudwatch(Canary::PConfig pConfig, ClientConfiguration* pClientConfig)
-    : pCanaryConfig(pConfig), logsClient(*pClientConfig), metricsClient(*pClientConfig)
+Cloudwatch::Cloudwatch(Canary::PConfig pConfig, ClientConfiguration* pClientConfig) : log(pConfig, pClientConfig)
 {
 }
 
@@ -14,41 +13,14 @@ STATUS Cloudwatch::init(Canary::PConfig pConfig)
     CreateLogGroupRequest createLogGroupRequest;
     Aws::CloudWatchLogs::Model::CreateLogStreamOutcome createLogStreamOutcome;
     CreateLogStreamRequest createLogStreamRequest;
-    BOOL useCloudwatchLogger = TRUE;
 
     clientConfig.region = pConfig->pRegion;
     auto& instance = getInstanceImpl(pConfig, &clientConfig);
 
-    createLogGroupRequest.SetLogGroupName(pConfig->pLogGroupName);
-    // ignore error since if this operation fails, CreateLogStream should fail as well.
-    // There might be some errors that can lead to successfull CreateLogStream, e.g. log group already exists.
-    instance.logsClient.CreateLogGroup(createLogGroupRequest);
-
-    createLogStreamRequest.SetLogGroupName(pConfig->pLogGroupName);
-    createLogStreamRequest.SetLogStreamName(pConfig->pLogStreamName);
-    createLogStreamOutcome = instance.logsClient.CreateLogStream(createLogStreamRequest);
-
-    if (!createLogStreamOutcome.IsSuccess()) {
-        useCloudwatchLogger = FALSE;
-        DLOGW("Failed to create \"%s\" log stream: %s", pConfig->pLogStreamName, createLogStreamOutcome.GetError().GetMessage().c_str());
-    }
-
-    if (useCloudwatchLogger) {
-        globalCustomLogPrintFn = logger;
-    } else {
+    if (STATUS_FAILED(instance.log.init())) {
         DLOGW("Failed to create Cloudwatch logger, fallback to local output");
-    }
-
-    instance.lock = MUTEX_CREATE(TRUE);
-    CHK(IS_VALID_MUTEX_VALUE(instance.lock), STATUS_INVALID_OPERATION);
-
-    instance.awaitPendingLogs = CVAR_CREATE();
-    CHK(IS_VALID_CVAR_VALUE(instance.awaitPendingLogs), STATUS_INVALID_OPERATION);
-
-CleanUp:
-
-    if (STATUS_FAILED(retStatus)) {
-        deinit();
+    } else {
+        globalCustomLogPrintFn = logger;
     }
 
     return retStatus;
@@ -65,97 +37,10 @@ Cloudwatch& Cloudwatch::getInstanceImpl(Canary::PConfig pConfig, ClientConfigura
     return instance;
 }
 
-STATUS Cloudwatch::deinit()
+VOID Cloudwatch::deinit()
 {
-    STATUS retStatus = STATUS_SUCCESS;
     auto& instance = getInstance();
-
-    instance.flush(TRUE);
-    if (IS_VALID_MUTEX_VALUE(instance.lock)) {
-        MUTEX_FREE(instance.lock);
-    }
-
-    if (IS_VALID_CVAR_VALUE(instance.awaitPendingLogs)) {
-        CVAR_FREE(instance.awaitPendingLogs);
-    }
-
-    return retStatus;
-}
-
-VOID Cloudwatch::pushLog(string log)
-{
-    MUTEX_LOCK(this->lock);
-    Aws::String awsCwString(log.c_str(), log.size());
-    auto logEvent =
-        Aws::CloudWatchLogs::Model::InputLogEvent().WithMessage(awsCwString).WithTimestamp(GETTIME() / HUNDREDS_OF_NANOS_IN_A_MILLISECOND);
-    this->logs.push_back(logEvent);
-
-    if (this->logs.size() >= MAX_CLOUDWATCH_LOG_COUNT) {
-        this->flush();
-    }
-    MUTEX_UNLOCK(this->lock);
-}
-
-VOID Cloudwatch::onPutLogEventResponseReceivedHandler(const Aws::CloudWatchLogs::CloudWatchLogsClient* cwClientLog,
-                                                      const Aws::CloudWatchLogs::Model::PutLogEventsRequest& request,
-                                                      const Aws::CloudWatchLogs::Model::PutLogEventsOutcome& outcome,
-                                                      const std::shared_ptr<const Aws::Client::AsyncCallerContext>& context)
-{
-    UNUSED_PARAM(cwClientLog);
-    UNUSED_PARAM(request);
-    UNUSED_PARAM(context);
-    auto& instance = getInstance();
-
-    if (!outcome.IsSuccess()) {
-        DLOGE("Failed to push logs: %s", outcome.GetError().GetMessage().c_str());
-    } else {
-        DLOGS("Successfully pushed logs to cloudwatch");
-        instance.token = outcome.GetResult().GetNextSequenceToken();
-    }
-
-    ATOMIC_STORE_BOOL(&instance.hasPendingLogs, FALSE);
-    CVAR_SIGNAL(instance.awaitPendingLogs);
-}
-
-VOID Cloudwatch::flush(BOOL sync)
-{
-    MUTEX_LOCK(this->lock);
-    if (this->logs.size() == 0) {
-        MUTEX_UNLOCK(this->lock);
-        return;
-    }
-
-    auto pendingLogs = this->logs;
-    this->logs.clear();
-
-    // wait until previous logs have been flushed entirely
-    while (this->hasPendingLogs) {
-        CVAR_WAIT(this->awaitPendingLogs, this->lock, 9999999999999999);
-    }
-
-    auto request = Aws::CloudWatchLogs::Model::PutLogEventsRequest()
-                       .WithLogGroupName(this->pCanaryConfig->pLogGroupName)
-                       .WithLogStreamName(this->pCanaryConfig->pLogStreamName)
-                       .WithLogEvents(pendingLogs);
-
-    if (this->token != "") {
-        request.SetSequenceToken(this->token);
-    }
-
-    if (!sync) {
-        ATOMIC_STORE_BOOL(&this->hasPendingLogs, TRUE);
-        this->logsClient.PutLogEventsAsync(request, onPutLogEventResponseReceivedHandler);
-    } else {
-        auto outcome = this->logsClient.PutLogEvents(request);
-        if (!outcome.IsSuccess()) {
-            DLOGE("Failed to push logs: %s", outcome.GetError().GetMessage().c_str());
-        } else {
-            DLOGS("Successfully pushed logs to cloudwatch");
-            getInstance().token = outcome.GetResult().GetNextSequenceToken();
-        }
-    }
-
-    MUTEX_UNLOCK(this->lock);
+    instance.log.deinit();
 }
 
 VOID Cloudwatch::logger(UINT32 level, PCHAR tag, PCHAR fmt, ...)
@@ -176,7 +61,111 @@ VOID Cloudwatch::logger(UINT32 level, PCHAR tag, PCHAR fmt, ...)
         va_start(valist, fmt);
         vprintf(logFmtString, valist);
         va_end(valist);
-        getInstance().pushLog(cwLogFmtString);
+        getInstance().log.push(cwLogFmtString);
+    }
+}
+
+CloudwatchLogger::CloudwatchLogger(PConfig pConfig, ClientConfiguration* pClientConfig) : pConfig(pConfig), client(*pClientConfig)
+{
+}
+
+STATUS CloudwatchLogger::init()
+{
+    STATUS retStatus = STATUS_SUCCESS;
+    CreateLogGroupRequest createLogGroupRequest;
+    Aws::CloudWatchLogs::Model::CreateLogStreamOutcome createLogStreamOutcome;
+    CreateLogStreamRequest createLogStreamRequest;
+
+    createLogGroupRequest.SetLogGroupName(pConfig->pLogGroupName);
+    // ignore error since if this operation fails, CreateLogStream should fail as well.
+    // There might be some errors that can lead to successfull CreateLogStream, e.g. log group already exists.
+    this->client.CreateLogGroup(createLogGroupRequest);
+
+    createLogStreamRequest.SetLogGroupName(pConfig->pLogGroupName);
+    createLogStreamRequest.SetLogStreamName(pConfig->pLogStreamName);
+    createLogStreamOutcome = this->client.CreateLogStream(createLogStreamRequest);
+
+    if (!createLogStreamOutcome.IsSuccess()) {
+        CHK_ERR(FALSE, STATUS_INTERNAL_ERROR, "Failed to create \"%s\" log stream: %s", pConfig->pLogStreamName,
+                createLogStreamOutcome.GetError().GetMessage().c_str());
+    }
+
+CleanUp:
+
+    return retStatus;
+}
+
+VOID CloudwatchLogger::deinit()
+{
+    this->flush(TRUE);
+}
+
+VOID CloudwatchLogger::push(string log)
+{
+    std::lock_guard<std::recursive_mutex> lock(this->sync.mutex);
+    Aws::String awsCwString(log.c_str(), log.size());
+    auto logEvent =
+        Aws::CloudWatchLogs::Model::InputLogEvent().WithMessage(awsCwString).WithTimestamp(GETTIME() / HUNDREDS_OF_NANOS_IN_A_MILLISECOND);
+    this->logs.push_back(logEvent);
+
+    if (this->logs.size() >= MAX_CLOUDWATCH_LOG_COUNT) {
+        this->flush();
+    }
+}
+
+VOID CloudwatchLogger::flush(BOOL sync)
+{
+    std::unique_lock<std::recursive_mutex> lock(this->sync.mutex);
+    if (this->logs.size() == 0) {
+        return;
+    }
+
+    auto pendingLogs = this->logs;
+    this->logs.clear();
+
+    // wait until previous logs have been flushed entirely
+    auto waitUntilFlushed = [this] { return !this->sync.pending.load(); };
+    this->sync.await.wait(lock, waitUntilFlushed);
+
+    auto request = Aws::CloudWatchLogs::Model::PutLogEventsRequest()
+                       .WithLogGroupName(this->pConfig->pLogGroupName)
+                       .WithLogStreamName(this->pConfig->pLogStreamName)
+                       .WithLogEvents(pendingLogs);
+
+    if (this->token != "") {
+        request.SetSequenceToken(this->token);
+    }
+
+    if (!sync) {
+        auto asyncHandler = [this](const Aws::CloudWatchLogs::CloudWatchLogsClient* cwClientLog,
+                                   const Aws::CloudWatchLogs::Model::PutLogEventsRequest& request,
+                                   const Aws::CloudWatchLogs::Model::PutLogEventsOutcome& outcome,
+                                   const std::shared_ptr<const Aws::Client::AsyncCallerContext>& context) {
+            UNUSED_PARAM(cwClientLog);
+            UNUSED_PARAM(request);
+            UNUSED_PARAM(context);
+
+            if (!outcome.IsSuccess()) {
+                DLOGE("Failed to push logs: %s", outcome.GetError().GetMessage().c_str());
+            } else {
+                DLOGS("Successfully pushed logs to cloudwatch");
+                this->token = outcome.GetResult().GetNextSequenceToken();
+            }
+
+            this->sync.pending = TRUE;
+            this->sync.await.notify_one();
+        };
+
+        this->sync.pending = FALSE;
+        this->client.PutLogEventsAsync(request, asyncHandler);
+    } else {
+        auto outcome = this->client.PutLogEvents(request);
+        if (!outcome.IsSuccess()) {
+            DLOGE("Failed to push logs: %s", outcome.GetError().GetMessage().c_str());
+        } else {
+            DLOGS("Successfully pushed logs to cloudwatch");
+            this->token = outcome.GetResult().GetNextSequenceToken();
+        }
     }
 }
 
--- webrtc-c/canary/src/Cloudwatch.h
+++ webrtc-c/canary/src/Cloudwatch.h
@@ -2,41 +2,46 @@
 
 namespace Canary {
 
+class Synchronization {
+  public:
+    std::atomic<bool> pending;
+    std::recursive_mutex mutex;
+    std::condition_variable_any await;
+};
+
+class CloudwatchLogger {
+  public:
+    CloudwatchLogger(Canary::PConfig, ClientConfiguration*);
+    STATUS init();
+    VOID deinit();
+    VOID push(string log);
+    VOID flush(BOOL sync = FALSE);
+
+  private:
+    PConfig pConfig;
+    CloudWatchLogsClient client;
+    Synchronization sync;
+    Aws::Vector<InputLogEvent> logs;
+    Aws::String token;
+};
+
 class Cloudwatch {
   public:
     Cloudwatch() = delete;
     Cloudwatch(Cloudwatch const&) = delete;
     void operator=(Cloudwatch const&) = delete;
 
+    CloudwatchLogger log;
+
     static Cloudwatch& getInstance();
     static STATUS init(Canary::PConfig);
-    static STATUS deinit();
+    static VOID deinit();
     static VOID logger(UINT32, PCHAR, PCHAR, ...);
-
-    VOID pushLog(string log);
-    VOID flush(BOOL sync = FALSE);
 
   private:
     static Cloudwatch& getInstanceImpl(Canary::PConfig = nullptr, ClientConfiguration* = nullptr);
 
     Cloudwatch(Canary::PConfig, ClientConfiguration*);
-
-    static VOID onPutLogEventResponseReceivedHandler(const Aws::CloudWatchLogs::CloudWatchLogsClient*,
-                                                     const Aws::CloudWatchLogs::Model::PutLogEventsRequest&,
-                                                     const Aws::CloudWatchLogs::Model::PutLogEventsOutcome&,
-                                                     const std::shared_ptr<const Aws::Client::AsyncCallerContext>&);
-
-    Canary::PConfig pCanaryConfig;
-    CloudWatchLogsClient logsClient;
-    CloudWatchClient metricsClient;
-
-    Aws::Vector<InputLogEvent> logs;
-    Aws::String token;
-
-    Aws::Vector<InputLogEvent> pendingLogs;
-    volatile ATOMIC_BOOL hasPendingLogs;
-    CVAR awaitPendingLogs;
-    MUTEX lock;
 };
 typedef Cloudwatch* PCloudwatch;
 
